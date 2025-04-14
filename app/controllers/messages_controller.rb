7f1a963e84cd349aca6200da23ca92ef39812a04
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-04-14 17:54:19 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,8 +11,6 @@ class MessagesController < ApplicationController
     room = Room.find(params[:room_id])
     message = room.messages.new(message_params)
     
-    binding.pry
-    # binding.pry
     if message.save
       redirect_to room_messages_path(room)
     else
