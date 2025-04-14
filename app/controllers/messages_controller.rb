8fbd59015d466b98d19870b8caadebc46595ba34
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-04-14 17:05:16 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,23 @@
 class MessagesController < ApplicationController
   def index
     @rooms = current_user.rooms
+    @current_room = Room.find_by(id: params[:room_id])
+    @message = Message.new
   end
+
+  def create
+    room = Room.find(params[:user_id])
+    message = room.message.new(message_params)
+    if message.save
+      redirect_to room_messages_path(room)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
